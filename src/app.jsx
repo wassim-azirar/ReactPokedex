8de3640bc8f0d8d52ae54a04a42710b541793340
Wassim AZIRAR <wassim.azirar@gmail.com>
2017-02-07 23:40:57 +0100

    Uncomment some code

--- src/app.jsx
+++ src/app.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import template from './app.rt';
-// import $ from 'jquery';
+import $ from 'jquery';
 
 export default class App extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //
-    //     this.state = {pokemons: []};
-    // }
-    //
-    // componentDidMount() {
-    //     this.App();
-    // }
-    //
-    // App() {
-    //     return $.getJSON('http://pokeapi.co/api/v2/pokemon/')
-    //         .then((data) => {
-    //             this.setState({pokemons: data.results});
-    //         });
-    // }
+    constructor(props) {
+        super(props);
 
-    add() {
-        console.log("hello");
-    };
+        this.state = {pokemons: []};
+    }
+
+    componentDidMount() {
+        this.App();
+    }
+
+    App() {
+        return $.getJSON('http://pokeapi.co/api/v2/pokemon/')
+            .then((data) => {
+                this.setState({pokemons: data.results});
+            });
+    }
 
     render() {
 
-        // const pokemon = this.state.pokemons.map((item) => {
-        //     return <div>
-        //         <h1>{item.name}</h1>
-        //         <span>{item.url}</span>
-        //     </div>
-        // });
-        //
-        // return <div>{ pokemon }</div>
+        const pokemon = this.state.pokemons.map((item) => {
+            return <div>
+                <h1>{item.name}</h1>
+            </div>
+        });
+
+        return <div>{pokemon}</div>
 
         return template()
     }
